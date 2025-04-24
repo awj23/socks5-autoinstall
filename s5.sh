2a9ee939c22e2b6ba83ba5f3d06b178ce88bfd07
awj23 <88016876+awj23@users.noreply.github.com>
2025-04-24 17:30:25 +0800

    Update s5.sh

--- s5.sh
+++ s5.sh
@@ -5,46 +5,35 @@ export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 if [ -n "$(grep 'Aliyun Linux release' /etc/issue)" -o -e /etc/redhat-release ];then
     OS=CentOS
     [ -n "$(grep ' 7\.' /etc/redhat-release)" ] && CentOS_RHEL_version=7
-    [ -n "$(grep ' 6\.' /etc/redhat-release)" -o -n "$(grep 'Aliyun Linux release6 15' /etc/issue)" ] && CentOS_RHEL_version=6
-    [ -n "$(grep ' 5\.' /etc/redhat-release)" -o -n "$(grep 'Aliyun Linux release5' /etc/issue)" ] && CentOS_RHEL_version=5
-elif [ -n "$(grep 'Amazon Linux AMI release' /etc/issue)" -o -e /etc/system-release ];then
-    OS=CentOS
-    CentOS_RHEL_version=6
 elif [ -n "$(grep bian /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'Debian' ];then
     OS=Debian
     [ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
     Debian_version=$(lsb_release -sr | awk -F. '{print $1}')
-elif [ -n "$(grep Deepin /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'Deepin' ];then
-    OS=Debian
-    [ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
-    Debian_version=$(lsb_release -sr | awk -F. '{print $1}')
-elif [ -n "$(grep Ubuntu /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'Ubuntu' -o -n "$(grep 'Linux Mint' /etc/issue)" ];then
+elif [ -n "$(grep Ubuntu /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'Ubuntu' ];then
     OS=Ubuntu
     [ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
     Ubuntu_version=$(lsb_release -sr | awk -F. '{print $1}')
-    [ -n "$(grep 'Linux Mint 18' /etc/issue)" ] && Ubuntu_version=16
 else
     echo "不支持此操作系统，请联系作者！"
     exit 1
 fi
 
-# 安装基础工具
+# 安装必要的依赖
 if [[ ${OS} == Ubuntu || ${OS} == Debian ]]; then
     apt update -y
     apt install git unzip wget gcc g++ automake make libpam0g-dev libldap2-dev libsasl2-dev libssl-dev -y
 elif [[ ${OS} == CentOS ]]; then
+    yum groupinstall "Development Tools" -y
     yum install git unzip wget gcc gcc-c++ automake make pam-devel openldap-devel cyrus-sasl-devel openssl-devel -y
+    yum install chkconfig -y
 else
     echo "不支持此操作系统！"
     exit 1
 fi
 
-# 清理旧环境和配置新环境
-Clear() {
-    unInstall
-    echo "旧环境清理完毕！"
-    echo "安装Socks5所依赖的组件，请稍等..."
-}
+# 创建必要的目录
+mkdir -p /etc/opt/ss5/
+mkdir -p /etc/sysconfig/
 
 # 下载Socks5服务
 Download() {
@@ -66,7 +55,7 @@ InstallSock5() {
     make install
 }
 
-# 安装控制面板配置参数
+# 安装控制面板配置
 InstallPanel() {
     mv /root/Socks5/service.sh /etc/opt/ss5/
     mv /root/Socks5/user.sh /etc/opt/ss5/
@@ -86,8 +75,8 @@ InstallPanel() {
 
     # 添加开机启动
     chmod +x /etc/init.d/ss5
-    chkconfig --add ss5
-    chkconfig --level 345 ss5 on
+    systemctl enable ss5
+    systemctl start ss5
 
     # 确保 /var/run/ss5/ 目录存在
     if [ ! -d "/var/run/ss5/" ]; then
@@ -102,7 +91,7 @@ check() {
         exit 1
     else
         echo "Socks5安装完毕！"
-        service ss5 start
+        systemctl start ss5
         echo "默认用户名: $uname"
         echo "默认密码  : $upasswd"
         echo "默认端口  : $port"
@@ -110,7 +99,6 @@ check() {
 }
 
 # 执行各个步骤
-Clear
 Download
 InstallSock5
 InstallPanel
